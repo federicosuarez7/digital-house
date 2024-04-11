5b2a7bfb09c52b003bc7c9837680a420b193b1f6
federicosuarez7 <federico.saboretto@gmail.com>
2024-04-10 23:12:54 -0300

    Comenzamos con Javascript, tipos de variables y como imprimir por consola (Js)

---
+++ Professional Developer/Proyecto/Javascript/variables.js
@@ -0,0 +1,11 @@
+//Tipos de variables
+let var_numeros = 10;
+let var_booleano = true;
+let var_caracter = "@";
+let var_string = "Hola";
+let var_null = null;
+let var_undefined = undefined;
+
+//Como imprimir en consola
+console.log(var_string);
+console.log("var_numeros");
