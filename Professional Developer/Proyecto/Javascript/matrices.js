69dce8600fb147f86b801f49a9413b4538724e92
federicosuarez7 <federico.saboretto@gmail.com>
2024-04-28 23:46:51 -0300

    Filtrar elementos de las matrices (JS)

--- Professional Developer/Proyecto/Javascript/matrices.js
+++ Professional Developer/Proyecto/Javascript/matrices.js
@@ -20,4 +20,17 @@ for (let i = 0; i < matriz[0].length; i++) {
     console.log(matriz[0][i]);
 }
 
+// Filtrar elementos dentro de una matriz
+let matriz2 = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+for (let i = 0; i< matriz2.length; i++) {
+    for (let j = 0; j < matriz2.length; j++) {
+        if (matriz2[i][j]%2 ==0) {
+            console.log(matriz2[i][j]);
+        }
+    }
+}
 
