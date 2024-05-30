d72a52566d37655db089b2059ebaa2ed8bc1798f
federicosuarez7 <federico.saboretto@gmail.com>
2024-05-29 23:38:13 -0300

    Manipulamos el DOM y modificamos elementos de html para hacer interactiva la web (JS)

---
+++ Professional Developer/Proyecto/Javascript para Dom/dom.js
@@ -0,0 +1,3 @@
+const salutations = document.querySelectorAll("p.saludos"); 
+document.getElementById("test").innerHTML = "Agregamos el primer saludo desde javascript: <br>" + 
+salutations[0].innerHTML;
