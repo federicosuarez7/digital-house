279b0a7c0b89e3870135f4477797ced7130466ed
federicosuarez7 <federico.saboretto@gmail.com>
2024-05-29 23:08:10 -0300

    Vemos ciclo forin (JS)

--- Professional Developer/Proyecto/Javascript para Dom/script.js
+++ Professional Developer/Proyecto/Javascript para Dom/script.js
@@ -1,5 +1,17 @@
 // Ciclo For of
+// Me sirve para recorrer objetos iterables como arrays, strings, maps
 let tecnologia = [ "Html","Javascript","Css","React Js"];
 for (let tech of tecnologia) {
         console.log(tech)
+}
+
+// Ciclo forin
+// Me sirve para recorrer las propiedades de los objetos
+let persona = {
+    nombre: "Federico",
+    edad:34,
+    ocupacion: "Comerciante"
+}
+for (let key in persona) {
+    console.log(persona[key])
 }
