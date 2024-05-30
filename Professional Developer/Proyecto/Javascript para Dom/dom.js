541c6d75647a15084b1c20af06548e0d36f35945
federicosuarez7 <federico.saboretto@gmail.com>
2024-05-29 23:49:31 -0300

    Manipulamos el DOM y modificamos elementos del formulario en html(JS)

--- Professional Developer/Proyecto/Javascript para Dom/dom.js
+++ Professional Developer/Proyecto/Javascript para Dom/dom.js
@@ -1,3 +1,13 @@
+// Manipulamos etiquetas de html desde el DOM
 const salutations = document.querySelectorAll("p.saludos"); 
 document.getElementById("test").innerHTML = "Agregamos el primer saludo desde javascript: <br>" + 
 salutations[0].innerHTML;
+
+// Manipulamos el formulario desde el DOM
+const form = document.forms["form1"];
+let text = "";
+for(let i=0; i<form.length;i++){
+    text += form.elements[i].value + '<br>';
+}
+
+document.getElementById('test2').innerHTML = text;
