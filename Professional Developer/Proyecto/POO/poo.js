9d18f7c4b79b489dff4f10e91e4873152252701a
Federico Suarez <federicosuarez.seosem@gmail.com>
2024-09-11 18:37:25 -0500

    Creamos una clase Persona con su constructor y metodos, la instanciamos e interactuamos con el DOM para mostrar el resultado en el HTML (JS)

--- Professional Developer/Proyecto/POO/poo.js
+++ Professional Developer/Proyecto/POO/poo.js
@@ -7,10 +7,26 @@ class Persona {
     }
     // Metodo de clase
     saludar(nombre){
-        console.log(`${this.nombre} dice:!Hola ${nombre}, ¿cómo estás?`);
+        return `${this.nombre} dice:<br>!Hola ${nombre}, ¿cómo estás?`;
+    }
+    decirEdad(edad){
+        return `${this.nombre} dice:<br>Yo tengo ${this.edad} años... `;
     }
 }
 
-// Instanciamos un objeto de la clase Persona
-let persona1 = new Persona("Federico",35);
-persona1.saludar("Yury");
+function saludar(){
+    const nombreInput = document.getElementById('nombreInput').value;
+    const edadInput = document.getElementById('edadInput').value;
+    const invitadoInput = document.getElementById('invitadoInput').value;
+
+    if(nombreInput.trim() == '' || edadInput.trim() == '' ){
+        alert('Los campos son obligatorios');
+        return;
+    }
+
+    const persona1 = new Persona(nombreInput,edadInput);
+    const saludo = persona1.saludar(invitadoInput);
+    const edadMsj = persona1.decirEdad(invitadoInput);
+    const resultado = document.getElementById('res');
+    resultado.innerHTML =  `<p> ${saludo}</p> <p>${edadMsj}</p>`;
+}
